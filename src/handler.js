d8ae3ec89898794273f4fb450691ed77e77716d5
Adam A Prasetya <adamworks23@gmail.com>
2022-07-24 21:50:50 +0700

    [core][handler] - delete parameters get all notes

--- src/handler.js
+++ src/handler.js
@@ -35,7 +35,7 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-const getAllNotesHandler = (request, h) => ({
+const getAllNotesHandler = () => ({
   status: 'success',
   data: {
     notes,
