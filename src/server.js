117f5ce4adab1ffce022574935770ab790d72c6c
Adam A Prasetya <adamworks23@gmail.com>
2022-07-24 12:39:14 +0700

    [core][server] - use 0 host

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const init = async () => {
   const server = Hapi.Server({
     port: 5000,
     // host: 'localhost',
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
