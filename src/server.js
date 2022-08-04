be75a94f7a04ef033ae801e894cef7bdf70ee4b2
Adam A Prasetya <adamworks23@gmail.com>
2022-08-04 07:39:38 +0700

    [CORE][PQSQL DB] - membuat konfigurasi db psql dan migration

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const Bugsnag = require('@bugsnag/js');
 const notes = require('./api/notes');
@@ -7,8 +9,9 @@ const NotesValidator = require('./validator/notes');
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
